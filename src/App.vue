45bff53d76ff050ab614fb8c827bda52a7cfc66d
Swathi657 <swathi.koratla@patharictech.com>
2021-07-19 17:30:24 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+<Counter/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Counter from './components/Counter.vue'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Welcome to Counter</h2>
+    {{counter}}
+    <br>
+    <button @click="incrementCounter">increment counter</button>
+  </div>
+</template>
+<script>
+export default {
+    methods: {
+      incrementCounter () {
+        this.$store.dispatch('inrementAction', 1)
+      }
+    },
+    computed: {
+      counter () {
+        return this.$store.getters.counter
+      }
+    }
+  }
+</script>
